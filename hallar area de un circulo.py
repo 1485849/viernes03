9e1788d1b36db1510918015df6e19397b0a6dfda
145849 <1485849@senati.pe>
2023-03-03 09:39:10 -0500

    resultado

--- hallar area de un circulo.py
+++ hallar area de un circulo.py
@@ -4,4 +4,6 @@
 
 import math
 print("ingrese el radio del circulo: ")
-r=float(input()) # el  radio
+r = float(input()) # el  radio
+a = math.pi * (r *r)
+print("el area del circulo es: ",round(a, 2))
