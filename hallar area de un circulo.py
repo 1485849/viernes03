c19dbca44e31aa479813d9e42d4eafd5d3eb2e47
145849 <1485849@senati.pe>
2023-03-03 09:35:37 -0500

    radio de un circulo

---
+++ hallar area de un circulo.py
@@ -0,0 +1,7 @@
+
+# Desarrolle un algoritmo para hallar el área de un circulo, 
+# el usuario debe ingresar el radio
+
+import math
+print("ingrese el radio del circulo: ")
+r=float(input()) # el  radio
