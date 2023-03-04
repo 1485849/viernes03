14c36257b6422123f64b29c04661474ddfcb384e
145849 <1485849@senati.pe>
2023-03-04 08:56:31 -0500

    nnuevo metodo agregado

--- 30-30_11-13.py
+++ 30-30_11-13.py
@@ -1,6 +1,9 @@
 # desarrollar un algoritmo donde permita el ingreso de datos
+print("ingrese los numeros de la lista: ", 10, 15, 27, 100, 200)
+asignaturaslista1 = ['10', '15', '27', '100', '200']
+asignaturalista2 = ('10', '15', '27', '100', '200')
+print(asignaturaslista1)
+print(asignaturalista2)
 
-lista = 10, 15, 27, 100, 200
-print(lista)
-print(lista,"")
+
 
