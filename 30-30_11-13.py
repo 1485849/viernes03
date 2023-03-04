87dd31e115f376136935f70a900b0bbc2c79df28
145849 <1485849@senati.pe>
2023-03-04 09:21:09 -0500

    primer ejercicio del 04-03-23

--- 30-30_11-13.py
+++ 30-30_11-13.py
@@ -1,9 +1,11 @@
 # desarrollar un algoritmo donde permita el ingreso de datos
-print("ingrese los numeros de la lista: ", 10, 15, 27, 100, 200)
+print("ingrese los numeros de la lista: ",)
 asignaturaslista1 = ['10', '15', '27', '100', '200']
 asignaturalista2 = ('10', '15', '27', '100', '200')
 print(asignaturaslista1)
 print(asignaturalista2)
+
+
 
 
 
---
+++ imprimir primer valor y ultimo de la lista.py
@@ -0,0 +1,4 @@
+# Escriba un código de programación donde puedan imprimir el primer
+# y ultimo valor de la lista.  
+
+lista = [ "red", "blue", "yellow", "black","white"]
